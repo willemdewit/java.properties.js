a9eb45a91343e4e2ba667566d8ba0eb73e538e15
Willem de Wit <willem_wit@hotmail.com>
2016-01-27 20:51:20 +0100

    add tonic example

---
+++ example/tonic.js
@@ -0,0 +1,17 @@
+var propertiesToObject = require('../dist/cjs/java.properties').default;
+
+var properties = '# i18n messages \n\
+user.edit.title             = Edit User\n\
+user.followers.title.one    = One Follower\n\
+user.followers.title.other  = All {{count}} Followers\n\
+button.add_user.title       = Add a user\n\
+button.add_user.text        = Add\n\
+button.add_user.disabled    = Saving...\n\
+# You can add comments like this,\n\
+    ! or with comments\n\
+    longvalue                   = You can even use \\\n\
+multi-line strings!';
+
+var parsed = propertiesToObject(properties);
+
+console.log(parsed);
